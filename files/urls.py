74509c64f1c1032eaf02aa44c191a5ae7ed30c26
Anton Barkovsky <anton@swarmer.me>
2015-06-18 21:37:52 +0300

    Add redirect to admin from /

--- files/urls.py
+++ files/urls.py
@@ -1,10 +1,12 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
 from django.views.static import serve
+from django.views.generic import RedirectView
 from django.conf import settings
 
 urlpatterns = patterns('',
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^$', RedirectView.as_view(url='/admin/', permanent=False)),
 )
 
 if settings.DEBUG:
